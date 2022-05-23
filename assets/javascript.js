298ec2863b1124916a112b1f3ba19724a2488627
Moraru <tiltiobusiness@gmail.com>
2022-05-23 17:14:17 +0300

    Revert "gg"
    This reverts commit db7fe54a38567584d60735719f9f7eba84fc60a7.

---
+++ assets/javascript.js
@@ -0,0 +1,16 @@
+
+
+
+var delayInMilliseconds = 1200;
+
+window.onload = setTimeout(() => {
+	document.querySelector(".preloader").style.zIndex = "-1";
+	document.querySelector(".preloader").style.opacity = "0";
+	document.querySelector(".body").style.overflow = "initial";
+
+}, delayInMilliseconds);
+
+
+
+
+
--- src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from "../store/reducers";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
